6c19452e76302a68bbf402c719429173683edda0
Brian Cassidy <bricas@cpan.org>
2008-04-08 15:03:14 +0000

    Import of BRICAS/WebService-Lucene-0.08 from CPAN.
    gitpan-cpan-distribution: WebService-Lucene
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         BRICAS/WebService-Lucene-0.08.tar.gz
    gitpan-cpan-author:       BRICAS
    gitpan-cpan-maturity:     released

--- lib/WebService/Lucene.pm
+++ lib/WebService/Lucene.pm
@@ -12,7 +12,7 @@ use WebService::Lucene::XOXOParser;
 use XML::LibXML;
 use Scalar::Util ();
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 __PACKAGE__->mk_accessors(
     qw(
@@ -222,7 +222,8 @@ sub _parse_service_document {
     my ( $workspace )
         = $doc->documentElement->getChildrenByTagName( 'workspace' );
 
-    $self->title_info( $workspace->getAttributeNode( 'title' )->value );
+    my( $title ) = $workspace->getElementsByLocalName( 'title' );
+    $self->title_info( $title->textContent );
 
     for my $collection ( $workspace->getChildrenByTagName( 'collection' ) ) {
         my $url = $collection->getAttributeNode( 'href' )->value;
